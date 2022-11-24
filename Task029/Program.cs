0248f219bdc5c16b42bb13a0d8be3657e1a3191b
Vyugar <vyugar@yandex.ru>
2022-11-25 02:00:05 +0300

    aded Task029

---
+++ Task029/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.
+*/
+
+int [] getRange(int length)
+{
+    int [] array = new int [length];
+    Random rd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rd.Next(0, 9);
+    }
+    return array;
+}
+
+void printArray(int [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int [] array = getRange(8);
+printArray(array);
